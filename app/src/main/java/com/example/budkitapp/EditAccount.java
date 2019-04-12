d5a04b6d5155a9197c9d612e3f2c2a7dda04d7e4
kahfi ahadian <smutaqin62@gmail.com>
2019-04-12 18:08:00 +0700

    account

---
+++ app/src/main/java/com/example/budkitapp/EditAccount.java
@@ -0,0 +1,16 @@
+package com.example.budkitapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_account);
+
+
+
+    }
+}
