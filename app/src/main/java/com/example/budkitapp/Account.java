1bfb99281712ccab78551b5f9b76429707b3eb4a
kahfi ahadian <smutaqin62@gmail.com>
2019-04-12 18:21:33 +0700

    tesst account commit

--- app/src/main/java/com/example/budkitapp/Account.java
+++ app/src/main/java/com/example/budkitapp/Account.java
@@ -24,6 +24,7 @@ public class Account extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account, container, false);
+
     }
 
 }
